fe67995e3d6f08982d00230aecb2963b0325ba47
Matthew Wolfe <matthew.wolfe2@gmail.com>
2022-07-25 23:01:50 -0400

    add basepath

--- packages/docs/next.config.js
+++ packages/docs/next.config.js
@@ -3,6 +3,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   assetPrefix: isProduction ? '/bedrock-ui/' : '',
+  basePath: isProduction ? '/bedrock-ui/' : '',
   reactStrictMode: true,
   swcMinify: true,
 };
