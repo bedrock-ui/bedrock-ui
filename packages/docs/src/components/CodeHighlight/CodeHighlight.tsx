4cb9608fa17c4be53b88dcf34e806dd4c7d28da5
Matthew Wolfe <matthew.wolfe2@gmail.com>
2022-07-27 23:51:08 -0400

    fix bundle size with code highlight

--- packages/docs/src/components/CodeHighlight/CodeHighlight.tsx
+++ packages/docs/src/components/CodeHighlight/CodeHighlight.tsx
@@ -1,7 +1,10 @@
-import SyntaxHighlighter from 'react-syntax-highlighter';
-import { xcode } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
+import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
+import typescript from 'react-syntax-highlighter/dist/cjs/languages/hljs/typescript';
+import xcode from 'react-syntax-highlighter/dist/cjs/styles/hljs/xcode';
 
 import type { Props } from './CodeHighlight.types';
+
+SyntaxHighlighter.registerLanguage('typescript', typescript);
 
 const CUSTOM_STYLE = {
   fontSize: '16px',
