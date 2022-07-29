df495e80a557ce076b749e9d69b4cf898c976a5e
Matthew Wolfe <matthew.wolfe2@gmail.com>
2022-07-28 22:57:23 -0400

    fixes for window undefined

--- packages/breakpoints/src/types/breakpoints.ts
+++ packages/breakpoints/src/types/breakpoints.ts
@@ -2,7 +2,7 @@ export type BreakpointMatches = Record<string, boolean>;
 
 export interface BreakpointMediaQuery {
   breakpointKey: string;
-  mediaQuery: MediaQueryList;
+  mediaQuery: MediaQueryList | undefined;
 }
 
 export type Breakpoints = Record<string, number>;
