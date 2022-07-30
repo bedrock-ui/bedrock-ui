df31e4158bb2c4ef56d5e3107b914c4f1fcc067e
Matthew Wolfe <matthew.wolfe2@gmail.com>
2022-07-30 19:36:54 -0400

    more components docs

--- packages/docs/pages/components/select.tsx
+++ packages/docs/pages/components/select.tsx
@@ -1,3 +1,4 @@
+import { Grid, Heading, Select } from '@bedrock-ui/core';
 import { CodeHighlight } from 'components/CodeHighlight';
 import { ComponentDocumentation } from 'components/ComponentDocumentation';
 
@@ -5,10 +6,88 @@ const CODE = `
 import { Select } from '@bedrock-ui/core';
 `.trim();
 
+const SELECT_CODE = `
+<Select>
+  <Select.Option>Lorem ipsum</Select.Option>
+  <Select.Option>dolor sit amet</Select.Option>
+  <Select.Option>consectetur adipiscing elit</Select.Option>
+</Select>
+`.trim();
+
+const SELECT_ERROR_CODE = `
+<Select error>
+  <Select.Option>Lorem ipsum</Select.Option>
+  <Select.Option>dolor sit amet</Select.Option>
+  <Select.Option>consectetur adipiscing elit</Select.Option>
+</Select>
+`.trim();
+
+const SELECT_SUCCESS_CODE = `
+<Select success>
+  <Select.Option>Lorem ipsum</Select.Option>
+  <Select.Option>dolor sit amet</Select.Option>
+  <Select.Option>consectetur adipiscing elit</Select.Option>
+</Select>
+`.trim();
+
 function ComponentsSelect() {
   return (
     <ComponentDocumentation name="Select">
       <CodeHighlight>{CODE}</CodeHighlight>
+
+      <Grid style={{ marginTop: 64, marginBottom: 64 }}>
+        <Grid.Col span={12}>
+          <Heading level={3}>Default</Heading>
+        </Grid.Col>
+
+        <Grid.Col span={12}>
+          <CodeHighlight>{SELECT_CODE}</CodeHighlight>
+        </Grid.Col>
+
+        <Grid.Col span={12}>
+          <Select style={{ maxWidth: 320 }}>
+            <Select.Option>Lorem ipsum</Select.Option>
+            <Select.Option>dolor sit amet</Select.Option>
+            <Select.Option>consectetur adipiscing elit</Select.Option>
+          </Select>
+        </Grid.Col>
+      </Grid>
+
+      <Grid style={{ marginTop: 64, marginBottom: 64 }}>
+        <Grid.Col span={12}>
+          <Heading level={3}>Error</Heading>
+        </Grid.Col>
+
+        <Grid.Col span={12}>
+          <CodeHighlight>{SELECT_ERROR_CODE}</CodeHighlight>
+        </Grid.Col>
+
+        <Grid.Col span={12}>
+          <Select error style={{ maxWidth: 320 }}>
+            <Select.Option>Lorem ipsum</Select.Option>
+            <Select.Option>dolor sit amet</Select.Option>
+            <Select.Option>consectetur adipiscing elit</Select.Option>
+          </Select>
+        </Grid.Col>
+      </Grid>
+
+      <Grid style={{ marginTop: 64, marginBottom: 64 }}>
+        <Grid.Col span={12}>
+          <Heading level={3}>Success</Heading>
+        </Grid.Col>
+
+        <Grid.Col span={12}>
+          <CodeHighlight>{SELECT_SUCCESS_CODE}</CodeHighlight>
+        </Grid.Col>
+
+        <Grid.Col span={12}>
+          <Select success style={{ maxWidth: 320 }}>
+            <Select.Option>Lorem ipsum</Select.Option>
+            <Select.Option>dolor sit amet</Select.Option>
+            <Select.Option>consectetur adipiscing elit</Select.Option>
+          </Select>
+        </Grid.Col>
+      </Grid>
     </ComponentDocumentation>
   );
 }
--- packages/docs/pages/components/textinput.tsx
+++ packages/docs/pages/components/textinput.tsx
@@ -1,3 +1,4 @@
+import { Grid, Heading, TextInput } from '@bedrock-ui/core';
 import { CodeHighlight } from 'components/CodeHighlight';
 import { ComponentDocumentation } from 'components/ComponentDocumentation';
 
@@ -5,10 +6,64 @@ const CODE = `
 import { TextInput } from '@bedrock-ui/core';
 `.trim();
 
+const TEXTINPUT_CODE = `
+<TextInput />
+`.trim();
+
+const TEXTINPUT_ERROR_CODE = `
+<TextInput error />
+`.trim();
+
+const TEXTINPUT_SUCCESS_CODE = `
+<TextInput success />
+`.trim();
+
 function ComponentsTextInput() {
   return (
     <ComponentDocumentation name="TextInput">
       <CodeHighlight>{CODE}</CodeHighlight>
+
+      <Grid style={{ marginTop: 64, marginBottom: 64 }}>
+        <Grid.Col span={12}>
+          <Heading level={3}>Default</Heading>
+        </Grid.Col>
+
+        <Grid.Col span={12}>
+          <CodeHighlight>{TEXTINPUT_CODE}</CodeHighlight>
+        </Grid.Col>
+
+        <Grid.Col span={12}>
+          <TextInput />
+        </Grid.Col>
+      </Grid>
+
+      <Grid style={{ marginBottom: 64 }}>
+        <Grid.Col span={12}>
+          <Heading level={3}>Error</Heading>
+        </Grid.Col>
+
+        <Grid.Col span={12}>
+          <CodeHighlight>{TEXTINPUT_ERROR_CODE}</CodeHighlight>
+        </Grid.Col>
+
+        <Grid.Col span={12}>
+          <TextInput error />
+        </Grid.Col>
+      </Grid>
+
+      <Grid style={{ marginBottom: 64 }}>
+        <Grid.Col span={12}>
+          <Heading level={3}>Success</Heading>
+        </Grid.Col>
+
+        <Grid.Col span={12}>
+          <CodeHighlight>{TEXTINPUT_SUCCESS_CODE}</CodeHighlight>
+        </Grid.Col>
+
+        <Grid.Col span={12}>
+          <TextInput success />
+        </Grid.Col>
+      </Grid>
     </ComponentDocumentation>
   );
 }
