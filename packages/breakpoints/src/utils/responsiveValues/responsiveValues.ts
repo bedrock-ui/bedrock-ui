a63095b57e397fb368563693abc42d32c3c90a7b
Matthew Wolfe <matthew.wolfe2@gmail.com>
2022-08-07 22:35:59 -0400

    update

--- packages/breakpoints/src/utils/responsiveValues/responsiveValues.ts
+++ packages/breakpoints/src/utils/responsiveValues/responsiveValues.ts
@@ -3,17 +3,8 @@ import type { BreakpointMatches } from 'types/breakpoints';
 function responsiveValues<T>(
   matches: BreakpointMatches,
   responsiveValues: Record<string, T>
-): T | undefined {
-  if (typeof window === 'undefined') {
-    return undefined;
-  }
-
-  const key = Object.keys(matches).find((match) => matches[match]);
-
-  if (!key) {
-    return undefined;
-  }
-
+): T {
+  const key = Object.keys(matches).find((match) => matches[match]) as string;
   return responsiveValues[key];
 }
 
