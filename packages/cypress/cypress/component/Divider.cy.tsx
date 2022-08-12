0e3e2822e47d220db24ad073323bc567c2c30ecd
Anis Tigrini <80918772+AnisTigrini@users.noreply.github.com>
2022-08-11 22:44:29 -0400

    Added Test Coverage For Divider (#14)
    * Added test coverage for divider
    
    * formatted with prettier

---
+++ packages/cypress/cypress/component/Divider.cy.tsx
@@ -0,0 +1,38 @@
+import '@bedrock-ui/core/css/bedrock-ui.css';
+
+import { Divider } from '@bedrock-ui/core';
+import { getElement, testCss } from 'utils';
+
+describe('Divider', () => {
+  it('default', () => {
+    cy.mount(<Divider />);
+
+    getElement('.divider', ({ css }) => {
+      testCss(css, {
+        width: `${document.body.clientWidth}px`,
+      });
+    });
+  });
+
+  it('dashed', () => {
+    cy.mount(<Divider variant="dashed" />);
+
+    getElement('.divider', ({ css }) => {
+      testCss(css, {
+        width: `${document.body.clientWidth}px`,
+        'border-style': 'dashed',
+      });
+    });
+  });
+
+  it('dotted', () => {
+    cy.mount(<Divider variant="dotted" />);
+
+    getElement('.divider', ({ css }) => {
+      testCss(css, {
+        width: `${document.body.clientWidth}px`,
+        'border-style': 'dotted',
+      });
+    });
+  });
+});
