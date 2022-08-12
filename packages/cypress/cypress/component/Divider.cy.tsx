cd24650ca5dfd61663a01f03eb28775af3085c1f
Anis Tigrini <anistigrini2@gmail.com>
2022-08-11 22:43:36 -0400

    formatted with prettier

--- packages/cypress/cypress/component/Divider.cy.tsx
+++ packages/cypress/cypress/component/Divider.cy.tsx
@@ -3,37 +3,36 @@ import '@bedrock-ui/core/css/bedrock-ui.css';
 import { Divider } from '@bedrock-ui/core';
 import { getElement, testCss } from 'utils';
 
-describe('Divider', () => {    
-    it("default", () => {
-        cy.mount(<Divider />);
+describe('Divider', () => {
+  it('default', () => {
+    cy.mount(<Divider />);
 
-        getElement(".divider", ({css}) => {
-            testCss(css, {
-                "width": `${document.body.clientWidth}px`,
-            })
-        })
-    })
+    getElement('.divider', ({ css }) => {
+      testCss(css, {
+        width: `${document.body.clientWidth}px`,
+      });
+    });
+  });
 
-    it("dashed", () => {
-        cy.mount(<Divider variant='dashed' />);
+  it('dashed', () => {
+    cy.mount(<Divider variant="dashed" />);
 
-        getElement(".divider", ({css}) => {
-            testCss(css, {
-                "width": `${document.body.clientWidth}px`,
-                "border-style": "dashed"
-            })
-        })
-    })
+    getElement('.divider', ({ css }) => {
+      testCss(css, {
+        width: `${document.body.clientWidth}px`,
+        'border-style': 'dashed',
+      });
+    });
+  });
 
-    it("dotted", () => {
-        cy.mount(<Divider variant="dotted"/>);
+  it('dotted', () => {
+    cy.mount(<Divider variant="dotted" />);
 
-        getElement(".divider", ({css}) => {
-            testCss(css, {
-                "width": `${document.body.clientWidth}px`,
-                "border-style": "dotted"
-            })
-        })
-    })
-
+    getElement('.divider', ({ css }) => {
+      testCss(css, {
+        width: `${document.body.clientWidth}px`,
+        'border-style': 'dotted',
+      });
+    });
+  });
 });
