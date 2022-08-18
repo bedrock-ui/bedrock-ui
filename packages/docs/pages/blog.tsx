66d2c24845dbb733e87cc226b1ca3d4bd2c1e7e6
Matthew Wolfe <matthew.wolfe2@gmail.com>
2022-08-17 22:22:10 -0400

    fix link

--- packages/docs/pages/blog.tsx
+++ packages/docs/pages/blog.tsx
@@ -1,12 +1,13 @@
+import NextLink from 'next/link';
 import { Link, Text } from '@bedrock-ui/core';
 import { BlogArticle } from 'components/BlogArticle';
 
 function Blog() {
   return (
     <BlogArticle title="Blog">
-      <Link href="/blog/reducing-bundle-size">
-        Reducing Bundle Size and Load Time With Bedrock UI
-      </Link>
+      <NextLink href="/blog/reducing-bundle-size" passHref>
+        <Link>Reducing Bundle Size and Load Time With Bedrock UI</Link>
+      </NextLink>
 
       <Text>A case study in refactoring a website built with Material UI to use Bedrock UI</Text>
     </BlogArticle>
