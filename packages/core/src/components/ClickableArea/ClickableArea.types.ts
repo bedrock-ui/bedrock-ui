57d187fba52b71b5e9d48be223de73c1facf4168
Matthew Wolfe <matthew.wolfe2@gmail.com>
2022-11-03 17:04:21 -0700

    add none

--- packages/core/src/components/ClickableArea/ClickableArea.types.ts
+++ packages/core/src/components/ClickableArea/ClickableArea.types.ts
@@ -1,5 +1,5 @@
 import type { Div } from 'types/elements';
 
 export interface Props extends Div {
-  color?: 'error' | 'info' | 'neutral' | 'primary' | 'success' | 'warning';
+  color?: 'error' | 'info' | 'neutral' | 'none' | 'primary' | 'success' | 'warning';
 }
