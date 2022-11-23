a29bce5cd645f6198853c2fde2a6e97523954597
Matthew Wolfe <matthew.wolfe2@gmail.com>
2022-11-22 19:48:02 -0800

    add text colors

--- packages/core/src/components/Text/Text.types.ts
+++ packages/core/src/components/Text/Text.types.ts
@@ -2,5 +2,5 @@ import type { MaybeBreakpointObject } from 'types/breakpoints';
 import type { Div } from 'types/elements';
 
 export interface Props extends Omit<Div, 'color'> {
-  color?: MaybeBreakpointObject<'default' | 'primary' | 'white'>;
+  color?: MaybeBreakpointObject<'default' | 'error' | 'neutral' | 'primary' | 'white'>;
 }
