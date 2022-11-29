eccec96d80cf9c4cd9106ccd4cc6bdf2ff55e4ff
Matthew Wolfe <matthew.wolfe2@gmail.com>
2022-11-28 17:01:39 -0700

    minor: add spacing component

---
+++ packages/core/src/components/Spacing/Spacing.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Text } from '../Text';
+import BedrockSpacing from './Spacing';
+
+import type { Props } from './Spacing.types';
+
+export default {
+  title: 'Spacing',
+  component: BedrockSpacing,
+  argTypes: {},
+};
+
+export const Spacing = (props: Props) => (
+  <>
+    <Text>Item 1</Text>
+    <BedrockSpacing {...props} />
+    <Text>Item 2</Text>
+  </>
+);
---
+++ packages/core/src/components/Spacing/Spacing.tsx
@@ -0,0 +1,22 @@
+import clsx from 'clsx';
+import React from 'react';
+
+import type { Props } from './Spacing.types';
+
+function Spacing({ className, h, w, ...props }: Props) {
+  return (
+    <div
+      {...props}
+      className={clsx(
+        'spacing',
+        {
+          [`h-${h}`]: h !== undefined,
+          [`w-${w}`]: w !== undefined,
+        },
+        className
+      )}
+    />
+  );
+}
+
+export default Spacing;
