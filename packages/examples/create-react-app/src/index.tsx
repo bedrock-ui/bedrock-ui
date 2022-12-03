7b9c74b7170b87a31a47b673fa69aaa04f96d1e4
Matthew Wolfe <matthew.wolfe2@gmail.com>
2022-12-03 13:45:03 -0700

    add create react app example

---
+++ packages/examples/create-react-app/src/index.tsx
@@ -0,0 +1,14 @@
+import 'assets/sass/bedrock-ui.scss';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from 'components/App';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
