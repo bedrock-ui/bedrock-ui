e5e61a21c85394620ad3f3a2af9a1c210d61937f
Matthew Wolfe <matthew.wolfe2@gmail.com>
2022-12-10 09:50:48 -0700

    major: convert all variables to sass

--- packages/core/scripts/copy-sass.mjs
+++ packages/core/scripts/copy-sass.mjs
@@ -1,4 +1,4 @@
 import fsExtra from 'fs-extra';
 import path from 'path';
 
-fsExtra.copySync(path.resolve(process.cwd(), 'src/css'), path.resolve(process.cwd(), 'sass'));
+fsExtra.copySync(path.resolve(process.cwd(), 'src/sass'), path.resolve(process.cwd(), 'sass'));
