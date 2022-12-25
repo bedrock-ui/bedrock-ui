e863ae4ad3c0b844af9bbedc91d86accfac09698
Matthew Wolfe <matthew.wolfe2@gmail.com>
2022-12-25 09:22:15 -0700

    zero grid gap

--- packages/core/src/components/Grid/Grid.types.ts
+++ packages/core/src/components/Grid/Grid.types.ts
@@ -2,7 +2,7 @@ import type { MaybeBreakpointObject } from 'types/breakpoints';
 import type { Div } from 'types/elements';
 
 type GridGaps = MaybeBreakpointObject<
-  1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16
+  0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16
 >;
 
 export interface ColProps extends Div {
