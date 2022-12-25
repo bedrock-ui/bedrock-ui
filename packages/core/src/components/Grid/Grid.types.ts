9214bad5243d37d72b8b4eaee5aa5126bf60214d
Matthew Wolfe <matthew.wolfe2@gmail.com>
2022-12-25 09:22:33 -0700

    update types

--- packages/core/src/components/Grid/Grid.types.ts
+++ packages/core/src/components/Grid/Grid.types.ts
@@ -1,10 +1,6 @@
 import type { MaybeBreakpointObject } from 'types/breakpoints';
 import type { Div } from 'types/elements';
 
-type GridGaps = MaybeBreakpointObject<
-  0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16
->;
-
 export interface ColProps extends Div {
   span: MaybeBreakpointObject<1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12>;
 }
@@ -12,13 +8,19 @@ export interface ColProps extends Div {
 export type Props = (GapProps | ColumnAndRowGapProps) & Div;
 
 interface GapProps {
-  gap: GridGaps;
+  gap: MaybeBreakpointObject<
+    0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16
+  >;
   columnGap?: never;
   rowGap?: never;
 }
 
 interface ColumnAndRowGapProps {
   gap?: never;
-  columnGap: GridGaps;
-  rowGap: GridGaps;
+  columnGap: MaybeBreakpointObject<
+    0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16
+  >;
+  rowGap: MaybeBreakpointObject<
+    0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16
+  >;
 }
